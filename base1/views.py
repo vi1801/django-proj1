25f196d46e00a3c061c7e3eae9b5d104d4420cd3
vi1801 <vrdalwadi18@gmail.com>
2024-11-17 22:59:22 -0800

    Django start

---
+++ base1/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, "home.html", {'name': 'Vi'})
+
+
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1 + val2
+    return render(request, "result.html", {"result": res})
